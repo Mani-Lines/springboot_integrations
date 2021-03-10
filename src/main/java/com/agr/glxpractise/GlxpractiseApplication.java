6c4b3de5ab59ea7a185057e439c24da406a316ea
Mani-Lines <manikantaammula1@gmail.com>
2021-03-10 15:52:17 +0530

    first commit

---
+++ src/main/java/com/agr/glxpractise/GlxpractiseApplication.java
@@ -0,0 +1,13 @@
+package com.agr.glxpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlxpractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlxpractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agr/glxpractise/GlxpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agr.glxpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlxpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
